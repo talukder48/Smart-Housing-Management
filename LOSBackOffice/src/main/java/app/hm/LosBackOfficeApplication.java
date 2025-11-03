9243ec43818e7a90e31e877f8e8d0654d1569ffb
Mosharraf Hossain Talukder <talukder.csedu@gmail.com>
2025-11-04 01:00:29 +0600

    commit

---
+++ LOSBackOffice/src/main/java/app/hm/LosBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package app.hm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosBackOfficeApplication.class, args);
+	}
+
+}
---
+++ LOSBackOffice/src/test/java/app/hm/LosBackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package app.hm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LosBackOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
